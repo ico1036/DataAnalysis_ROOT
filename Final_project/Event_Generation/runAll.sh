dc4676418a235e89506aad779f54b54c90ddd350
jkim <jkim@localhost.localdomain>
2018-12-13 18:24:44 +0900

    Final

--- Final_project/Event_Generation/runAll.sh
+++ Final_project/Event_Generation/runAll.sh
@@ -1,4 +1,15 @@
-./runDelphes_withPythia8.sh ZZto4l
-./runDelphes_withPythia8.sh Htozz
-./runDelphes_withPythia8.sh Z+jets
+#!/bin/bash
+
+
+for i in `seq 2 9`
+do
+
+./runDelphes_withPythia8.sh HtoZZto4l/Events/run_0$i 
+./runDelphes_withPythia8.sh BKG4l/Events/run_0$i 
+done
+
+./runDelphes_withPythia8.sh HtoZZto4l/Events/run_10 
+./runDelphes_withPythia8.sh BKG4l/Events/run_10 
+
+
 
--- Final_project/Event_Generation/runDelphes_withPythia8.sh
+++ Final_project/Event_Generation/runDelphes_withPythia8.sh
@@ -1,23 +1,29 @@
 #Locate this file in Delphes main directory
 
+############### Set input path
 
 filename=$1
+fullpath="/home/jkim/Generator/MG5_aMC_v2_6_4/Higss2z/$filename"
 
- 
-fullpath="/home/jkim/Generator/MG5_aMC_v2_6_4/Higss2z/$filename/Events/run_01"
+
+############### Find lhe file
 
 if [! -f $fullpath/unweighted_events.lhe.gz]; then echo '### No LHEfile found'; exit; fi
 gzip -d $fullpath/unweighted_events.lhe.gz
 ls $fullpath/unweighted_events.lhe
 
+############### Make temporary directory
+
 tempdir="pydelphes_tmp${RANDOM}_${RANDOM}"
 mkdir $tempdir
 cd $tempdir
 
+############### Write config.cmnd file
+
 cat << EOF > config.cmnd
 ! 1) Settings used in the main program.
 
-Main:numberOfEvents = 10000         ! number of events to generate
+Main:numberOfEvents = 10000            ! number of events to generate
 #Main:timesAllowErrors = 3          ! how many aborts before run stops
 
 ! 2) Settings related to output in init(), next() and stat().
@@ -35,7 +41,10 @@ Beams:frameType = 4
 Beams:LHEF = $fullpath/unweighted_events.lhe
 EOF
 
+############### Run DelphesPythia8 
+
 ../DelphesPythia8 ../cards/delphes_card_CMS.tcl config.cmnd tag_delphes.root
 cp tag_delphes.root $fullpath
 cd ..
-                                        
+rm -rf $tempdir
+
